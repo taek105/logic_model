42488567ed9e9d320136acacbb21a89159b331e0
ht5172 <ht5172@naver.com>
2025-02-22 17:37:54 +0900

    feat(app.py): add root message

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from init import xor_model, or_model, not_model
 
 from fastapi import FastAPI, Query
 
-# print(f"ormodel 0,0: {or_model.predict([0,0])}")
-# print(f"notmodel 1: {not_model.predict(1)}")
-# print(f"xormodel 1,1: {xor_model.predict([1,1])}")
-
 app = FastAPI()
+
+@app.get("/")
+def root():
+    """✅ 서버 접속 시 바로 연결 성공 메시지 반환"""
+    return {"message": "connection online"}
 
 @app.get("/or")
 def or_inference(x1: int = Query(), x2: int = Query()):
